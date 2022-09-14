ed75d529b4040ffeb79062600be8395198550f2a
zcming123 <33447327+zcming123@users.noreply.github.com>
2022-09-14 23:14:31 +0800

    数据库文件

---
+++ spring-data-jpa/src/main/resources/jpa.sql
@@ -0,0 +1,40 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 80025
+ Source Host           : localhost:3306
+ Source Schema         : jpa
+
+ Target Server Type    : MySQL
+ Target Server Version : 80025
+ File Encoding         : 65001
+
+ Date: 14/09/2022 23:13:29
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for tb_resume
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_resume`;
+CREATE TABLE `tb_resume`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of tb_resume
+-- ----------------------------
+INSERT INTO `tb_resume` VALUES (1, '北京', '张三', '131000000');
+INSERT INTO `tb_resume` VALUES (2, '上海', '李四', '151000000');
+INSERT INTO `tb_resume` VALUES (3, '⼴州', '王五', '153000000');
+INSERT INTO `tb_resume` VALUES (4, '成都', '赵六六', '132000000');
+
+SET FOREIGN_KEY_CHECKS = 1;
